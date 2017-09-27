ca4934ebaf3882f5e84a2ca9e86ce3e2f9f2d72c
Isabelle <Isabelle@LAPTOP-CN7VNT6D>
2017-09-26 21:23:55 -0300

    Commit

--- Avaliacao/Migrations/201709262336220_Tabela.cs
+++ Avaliacao/Migrations/201709262336220_Tabela.cs
@@ -21,7 +21,12 @@ namespace Avaliacao.Migrations
             Sql("INSERT INTO Faixas (Nome, Grau) VALUES ('Marrom', '1k')");
             Sql("INSERT INTO Faixas (Nome, Grau) VALUES ('Preta', '1d')");
 
-            
+            Sql("INSERT INTO Alunoes (Nome, Cpf, Faixa_Id, TipodeAssociacao_Id) VALUES ('Martim Castro Ribeiro', '158.778.735-04', 8, 17)");
+            Sql("INSERT INTO Alunoes (Nome, Cpf, Faixa_Id, TipodeAssociacao_Id) VALUES ('Lucas Alves Melo', '916.749.332-74', 11, 19)");
+            Sql("INSERT INTO Alunoes (Nome, Cpf, Faixa_Id, TipodeAssociacao_Id) VALUES ('Breno Castro Carvalho', '454.019.800-9', 9, 18)");
+            Sql("INSERT INTO Alunoes (Nome, Cpf, Faixa_Id, TipodeAssociacao_Id) VALUES ('Laura Cardoso Rocha', '361.089.665-56', 13, 20)");
+
+
 
         }
         
--- Avaliacao/Models/Faixa.cs
+++ Avaliacao/Models/Faixa.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿/* by Isabelle isabelle.minel@catolicasc.org.br */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
